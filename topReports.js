76cdfd63c3058b574c5c3f1f931b655aeee156bb
ShockHit <mr.edenhazani@gmail.com>
2020-03-15 15:01:28 +0200

    fixed top reports

--- topReports.js
+++ topReports.js
@@ -9,22 +9,25 @@ $(function() {
                 text: "Highest Coins"
             },
             axisY: {
-                title: "Volume"
+                title: "Volume",
+                labelFontSize: 14
+            },
+            axisX: {
+                labelFontSize: 12
             },
             data: [{
-                // Change type to "doughnut", "line", "splineArea", etc.
                 type: "column",
                 dataPoints: [
-                    { label: highCoinsName[0] + ` price USD: ${highCoinsWorth[0]} `, y: highCoinsVolume[0] },
-                    { label: highCoinsName[1] + ` price USD:${highCoinsWorth[1]} `, y: highCoinsVolume[1] },
-                    { label: highCoinsName[2] + ` price USD:${highCoinsWorth[2]} `, y: highCoinsVolume[2] },
-                    { label: highCoinsName[3] + ` price USD:${highCoinsWorth[3]} `, y: highCoinsVolume[3] },
-                    { label: highCoinsName[4] + ` price USD:${highCoinsWorth[4]} `, y: highCoinsVolume[4] },
-                    { label: highCoinsName[5] + ` price USD:${highCoinsWorth[5]} `, y: highCoinsVolume[5] },
-                    { label: highCoinsName[6] + ` price USD:${highCoinsWorth[6]} `, y: highCoinsVolume[6] },
-                    { label: highCoinsName[7] + ` price USD:${highCoinsWorth[7]} `, y: highCoinsVolume[7] },
-                    { label: highCoinsName[8] + ` price USD:${highCoinsWorth[8]} `, y: highCoinsVolume[8] },
-                    { label: highCoinsName[9] + ` price USD:${highCoinsWorth[9]} `, y: highCoinsVolume[9] }
+                    { label: highCoinsName[0] + ` price USD: ${highCoinsWorth[0]}$`, y: highCoinsVolume[0] },
+                    { label: highCoinsName[1] + ` price USD: ${highCoinsWorth[1]}$`, y: highCoinsVolume[1] },
+                    { label: highCoinsName[2] + ` price USD: ${highCoinsWorth[2]}$`, y: highCoinsVolume[2] },
+                    { label: highCoinsName[3] + ` price USD: ${highCoinsWorth[3]}$`, y: highCoinsVolume[3] },
+                    { label: highCoinsName[4] + ` price USD: ${highCoinsWorth[4]}$`, y: highCoinsVolume[4] },
+                    { label: highCoinsName[5] + ` price USD: ${highCoinsWorth[5]}$`, y: highCoinsVolume[5] },
+                    { label: highCoinsName[6] + ` price USD: ${highCoinsWorth[6]}$`, y: highCoinsVolume[6] },
+                    { label: highCoinsName[7] + ` price USD: ${highCoinsWorth[7]}$`, y: highCoinsVolume[7] },
+                    { label: highCoinsName[8] + ` price USD: ${highCoinsWorth[8]}$`, y: highCoinsVolume[8] },
+                    { label: highCoinsName[9] + ` price USD: ${highCoinsWorth[9]}$`, y: highCoinsVolume[9] }
                 ]
             }]
         });
